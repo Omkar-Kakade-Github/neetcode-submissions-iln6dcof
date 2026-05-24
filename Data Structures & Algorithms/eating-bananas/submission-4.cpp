a9d20afde93ef48fc7bd3d86ba3931882e304eaa
trainer_brendan <142827433+Omkar-Kakade-Github@users.noreply.github.com>
2026-05-24 21:44:52 +0530

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1, high = *max_element(piles.begin(), piles.end());
+        int opt_rate = 0;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int rate = 0;
+
+            for (int i: piles) {
+                rate += (i + mid - 1) / mid;;
+            }
+
+            if (rate > h) {
+                low = mid + 1;
+            }
+
+            else if (rate <= h) {
+                opt_rate = mid;
+                high = mid - 1;
+            }
+        }
+        return opt_rate;
+    }
+};
