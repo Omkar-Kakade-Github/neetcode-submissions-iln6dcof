4de080683638d3fa3df417b10a3481cde8a1d71c
trainer_brendan <142827433+Omkar-Kakade-Github@users.noreply.github.com>
2026-06-16 07:58:37 +0530

    Add: invert-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-1.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    TreeNode* switcher(TreeNode* root) {
+        TreeNode* temp;
+
+        temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+
+        return root;
+    }
+
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return root;
+        }
+
+        TreeNode* left_node;
+        TreeNode* right_node;
+    
+        root = switcher(root);
+        left_node = invertTree(root->left);
+        right_node = invertTree(root->right);
+
+        return root;
+    }
+};
+
+/**
+for every sub tree, we have to switch the left and right nodes.
+create a switch helper function which does this.
+temp = root->left->val
+root->left->val = root->right->val
+root->right->val = temp
+
+*/
